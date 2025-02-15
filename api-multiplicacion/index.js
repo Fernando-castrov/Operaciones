01888344a79ea9e2ca720ce8f819d5603fbc4999
Fernando Castro <fernando.castro@ambarseguros.mx>
2025-02-14 19:58:34 -0600

    Initial commit

---
+++ api-multiplicacion/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+
+// Configurar para que Express pueda recibir datos en formato JSON
+app.use(express.json());
+
+
+// Ruta POST para realizar la multiplicacion
+app.post('/multiplicacion', (req, res) => {
+    const { num1, num2 } = req.body;
+
+   
+    // Verificar que los números han sido enviados en el cuerpo de la solicitud
+    if (num1 === undefined || num2 === undefined) {
+        return res.status(400).json({ error: 'Faltan los parámetros num1 y num2' });
+    }
+
+    
+    // Realizar la multiplicacion
+    const resultado = num1 + num2;
+
+       // Enviar la respuesta con el resultado
+       res.json({ resultado });
+    });
+
+    
+// Iniciar el servidor
+const port = 3000;
+app.listen(port, () => {
+    console.log(`API escuchando en http://localhost:${port}`);
+});
+
+
+
